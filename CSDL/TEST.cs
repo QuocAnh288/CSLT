da79732b423141503e9b65a0ce2543f5686f8fcb
QuocAnh288 <quocanhvo472@gmail.com>
2026-08-11 23:59:11 +0700

    Thêm x

---
+++ CSDL/TEST.cs
@@ -0,0 +1,13 @@
+﻿internal class TEST
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        static int cong(int x)
+        { return x; };
+        cong(1);
+        Console.WriteLine(cong(1));
+    }
+}
