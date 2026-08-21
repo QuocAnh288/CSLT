d99505cb44509736b758ccbbaddf956eefba8348
QuocAnh288 <quocanhvo472@gmail.com>
2026-08-21 15:30:36 +0700

    Bài tập buổi 3

--- CSDL/SS3/Session3.cs
+++ CSDL/SS3/Session3.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using 
+
 namespace CSLT.SS3
 {
     internal class Session3
     {
         static void Main(string[] args)
         {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
             // Convert Celsius to Fahrenheit and Kelvin
             Console.WriteLine("Enter temperature in Celsius:");
             double c = double.Parse(Console.ReadLine());
@@ -36,6 +40,38 @@ namespace CSLT.SS3
             Console.WriteLine($"{a} * {b} = {a * b}");
             Console.WriteLine($"{a} / {b} = {a / b}");
             Console.WriteLine($"{a} % {b} = {a % b}");
+
+            // Bài tập 1 
+            Console.WriteLine($"Nhập số điện cũ:");
+            long SDC = long.Parse(Console.ReadLine());
+            Console.WriteLine($"Nhập số điện mới:");
+            long SDM = long.Parse(Console.ReadLine());
+            
+            bool check = SDM > SDC;
+            if (!check)
+            {
+                Console.WriteLine($"Số điện mới phải lớn hơn số điện cũ");
+                return;
+            }
+            long Dien = SDM - SDC;
+            Console.WriteLine($"Lượng điện tiêu thụ trong tháng: {Dien}");
+            if (Dien <= 50)
+            {
+                Console.WriteLine($"Tiền điện phải trả: {Dien * 1806}");
+            }
+            else if (Dien <= 100)
+            {
+                Console.WriteLine($"Tiền điện phải trả: {50 * 1806 + (Dien - 50) * 1866}");
+            }
+            else if (Dien <= 200)
+            {
+                Console.WriteLine($"Tiền điện phải trả: {50 * 1806 + 50 * 1866 + (Dien - 100) * 2167}");
+            }
+            else
+            {
+                Console.WriteLine($"Tiền điện phải trả: {50 * 1806 + 50 * 1866 + 100 * 2167 + (Dien - 200) * 2729}");
+            }
+            
         }
     }
 }
